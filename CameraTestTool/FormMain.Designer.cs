5fc6fb7800e3272ce716b9f8a23f4b76091a3cf7
umetsu <toshiyuki.umetsu@cosmosweb.com>
2023-07-14 09:47:20 +0900

    [Add]画像ファイルへの保存機能を追加

--- CameraTestTool/FormMain.Designer.cs
+++ CameraTestTool/FormMain.Designer.cs
@@ -18,16 +18,19 @@
         private void InitializeComponent()
         {
             this.buttonCapture = new System.Windows.Forms.Button();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBoxOpenCvParameter = new System.Windows.Forms.GroupBox();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
-            this.label1 = new System.Windows.Forms.Label();
             this.numericUpDownAutoExposure = new System.Windows.Forms.NumericUpDown();
             this.numericUpDownExposure = new System.Windows.Forms.NumericUpDown();
-            this.label2 = new System.Windows.Forms.Label();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.buttonCameraSetting = new System.Windows.Forms.Button();
             this.pictureBox = new System.Windows.Forms.PictureBox();
-            this.groupBox1.SuspendLayout();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.checkBoxExposureControl = new System.Windows.Forms.CheckBox();
+            this.checkBoxExposure = new System.Windows.Forms.CheckBox();
+            this.buttonChangeDevice = new System.Windows.Forms.Button();
+            this.groupBoxOpenCvParameter.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAutoExposure)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownExposure)).BeginInit();
@@ -38,7 +41,7 @@
             // buttonCapture
             // 
             this.buttonCapture.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.buttonCapture.Location = new System.Drawing.Point(73, 374);
+            this.buttonCapture.Location = new System.Drawing.Point(61, 374);
             this.buttonCapture.Name = "buttonCapture";
             this.buttonCapture.Size = new System.Drawing.Size(96, 69);
             this.buttonCapture.TabIndex = 0;
@@ -46,85 +49,67 @@
             this.buttonCapture.UseVisualStyleBackColor = true;
             this.buttonCapture.Click += new System.EventHandler(this.OnButtonCaptureClick);
             // 
-            // groupBox1
+            // groupBoxOpenCvParameter
             // 
-            this.tableLayoutPanel1.SetColumnSpan(this.groupBox1, 3);
-            this.groupBox1.Controls.Add(this.tableLayoutPanel2);
-            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBox1.Location = new System.Drawing.Point(3, 3);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(236, 336);
-            this.groupBox1.TabIndex = 3;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "OpenCVパラメータ";
-            this.groupBox1.Visible = false;
+            this.tableLayoutPanel1.SetColumnSpan(this.groupBoxOpenCvParameter, 3);
+            this.groupBoxOpenCvParameter.Controls.Add(this.tableLayoutPanel2);
+            this.groupBoxOpenCvParameter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBoxOpenCvParameter.Location = new System.Drawing.Point(3, 32);
+            this.groupBoxOpenCvParameter.Name = "groupBoxOpenCvParameter";
+            this.groupBoxOpenCvParameter.Size = new System.Drawing.Size(213, 307);
+            this.groupBoxOpenCvParameter.TabIndex = 3;
+            this.groupBoxOpenCvParameter.TabStop = false;
+            this.groupBoxOpenCvParameter.Text = "OpenCVパラメータ";
+            this.groupBoxOpenCvParameter.Visible = false;
             // 
             // tableLayoutPanel2
             // 
             this.tableLayoutPanel2.ColumnCount = 2;
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel2.Controls.Add(this.label1, 0, 0);
             this.tableLayoutPanel2.Controls.Add(this.numericUpDownAutoExposure, 1, 0);
             this.tableLayoutPanel2.Controls.Add(this.numericUpDownExposure, 1, 1);
-            this.tableLayoutPanel2.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.checkBoxExposureControl, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.checkBoxExposure, 0, 1);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 15);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
-            this.tableLayoutPanel2.RowCount = 3;
+            this.tableLayoutPanel2.RowCount = 5;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(230, 318);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(207, 289);
             this.tableLayoutPanel2.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, 6);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(160, 12);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "CAP_PROP_AUTO_EXPOSURE";
             // 
             // numericUpDownAutoExposure
             // 
             this.numericUpDownAutoExposure.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.numericUpDownAutoExposure.Location = new System.Drawing.Point(169, 3);
+            this.numericUpDownAutoExposure.Location = new System.Drawing.Point(127, 3);
             this.numericUpDownAutoExposure.Maximum = new decimal(new int[] {
             1,
             0,
             0,
             0});
             this.numericUpDownAutoExposure.Name = "numericUpDownAutoExposure";
-            this.numericUpDownAutoExposure.Size = new System.Drawing.Size(58, 19);
+            this.numericUpDownAutoExposure.Size = new System.Drawing.Size(77, 19);
             this.numericUpDownAutoExposure.TabIndex = 1;
             this.numericUpDownAutoExposure.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // numericUpDownExposure
             // 
             this.numericUpDownExposure.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.numericUpDownExposure.Location = new System.Drawing.Point(169, 28);
+            this.numericUpDownExposure.Location = new System.Drawing.Point(127, 28);
             this.numericUpDownExposure.Minimum = new decimal(new int[] {
             100,
             0,
             0,
             -2147483648});
             this.numericUpDownExposure.Name = "numericUpDownExposure";
-            this.numericUpDownExposure.Size = new System.Drawing.Size(58, 19);
+            this.numericUpDownExposure.Size = new System.Drawing.Size(77, 19);
             this.numericUpDownExposure.TabIndex = 2;
             this.numericUpDownExposure.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
-            // 
-            // label2
-            // 
-            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(3, 31);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(125, 12);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "CAP_PROP_EXPOSURE";
             // 
             // tableLayoutPanel1
             // 
@@ -132,23 +117,26 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Controls.Add(this.buttonCapture, 1, 2);
-            this.tableLayoutPanel1.Controls.Add(this.groupBox1, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.buttonCameraSetting, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.buttonCapture, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.groupBoxOpenCvParameter, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.buttonCameraSetting, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.buttonSave, 2, 3);
+            this.tableLayoutPanel1.Controls.Add(this.buttonChangeDevice, 0, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(572, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(595, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(242, 446);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(219, 446);
             this.tableLayoutPanel1.TabIndex = 4;
             // 
             // buttonCameraSetting
             // 
             this.buttonCameraSetting.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonCameraSetting.Location = new System.Drawing.Point(73, 345);
+            this.buttonCameraSetting.Location = new System.Drawing.Point(61, 345);
             this.buttonCameraSetting.Name = "buttonCameraSetting";
             this.buttonCameraSetting.Size = new System.Drawing.Size(96, 23);
             this.buttonCameraSetting.TabIndex = 4;
@@ -162,9 +150,57 @@
             this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pictureBox.Location = new System.Drawing.Point(0, 0);
             this.pictureBox.Name = "pictureBox";
-            this.pictureBox.Size = new System.Drawing.Size(572, 446);
+            this.pictureBox.Size = new System.Drawing.Size(595, 446);
             this.pictureBox.TabIndex = 5;
             this.pictureBox.TabStop = false;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.buttonSave.Enabled = false;
+            this.buttonSave.Location = new System.Drawing.Point(163, 420);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(53, 23);
+            this.buttonSave.TabIndex = 5;
+            this.buttonSave.Text = "保存";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.OnButtonSaveClick);
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.Filter = "JPEG形式(*.jpg)|*.jpg|PNG形式(*.png)|*.png|全てのファイル(*.*)|*.*";
+            // 
+            // checkBoxExposureControl
+            // 
+            this.checkBoxExposureControl.AutoSize = true;
+            this.checkBoxExposureControl.Location = new System.Drawing.Point(3, 3);
+            this.checkBoxExposureControl.Name = "checkBoxExposureControl";
+            this.checkBoxExposureControl.Size = new System.Drawing.Size(118, 16);
+            this.checkBoxExposureControl.TabIndex = 4;
+            this.checkBoxExposureControl.Text = "AUTO_EXPOSURE";
+            this.checkBoxExposureControl.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxExposure
+            // 
+            this.checkBoxExposure.AutoSize = true;
+            this.checkBoxExposure.Location = new System.Drawing.Point(3, 28);
+            this.checkBoxExposure.Name = "checkBoxExposure";
+            this.checkBoxExposure.Size = new System.Drawing.Size(83, 16);
+            this.checkBoxExposure.TabIndex = 5;
+            this.checkBoxExposure.Text = "EXPOSURE";
+            this.checkBoxExposure.UseVisualStyleBackColor = true;
+            // 
+            // buttonChangeDevice
+            // 
+            this.buttonChangeDevice.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.tableLayoutPanel1.SetColumnSpan(this.buttonChangeDevice, 3);
+            this.buttonChangeDevice.Location = new System.Drawing.Point(141, 3);
+            this.buttonChangeDevice.Name = "buttonChangeDevice";
+            this.buttonChangeDevice.Size = new System.Drawing.Size(75, 23);
+            this.buttonChangeDevice.TabIndex = 6;
+            this.buttonChangeDevice.Text = "カメラを変更する";
+            this.buttonChangeDevice.UseVisualStyleBackColor = true;
+            this.buttonChangeDevice.Click += new System.EventHandler(this.OnButtonChangeDevieClick);
             // 
             // FormMain
             // 
@@ -180,7 +216,7 @@
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.OnFormClosed);
             this.Load += new System.EventHandler(this.OnFormLoad);
             this.Shown += new System.EventHandler(this.OnFormShown);
-            this.groupBox1.ResumeLayout(false);
+            this.groupBoxOpenCvParameter.ResumeLayout(false);
             this.tableLayoutPanel2.ResumeLayout(false);
             this.tableLayoutPanel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAutoExposure)).EndInit();
@@ -194,15 +230,18 @@
         #endregion
 
         private System.Windows.Forms.Button buttonCapture;
-        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBoxOpenCvParameter;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.NumericUpDown numericUpDownAutoExposure;
         private System.Windows.Forms.NumericUpDown numericUpDownExposure;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.PictureBox pictureBox;
         private System.Windows.Forms.Button buttonCameraSetting;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.CheckBox checkBoxExposureControl;
+        private System.Windows.Forms.CheckBox checkBoxExposure;
+        private System.Windows.Forms.Button buttonChangeDevice;
     }
 }
 
--- CameraTestTool/FormMain.cs
+++ CameraTestTool/FormMain.cs
@@ -9,19 +9,23 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 using OpenCvSharp;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace CameraTestTool
 {
     public partial class FormMain : Form
     {
+        /// <summary>
+        /// 新しいインスタンスを構築する。
+        /// </summary>
         public FormMain()
         {
             InitializeComponent();
 
             { // フォームタイトル
                 StringBuilder title = new StringBuilder();
-                title.Append(Application.ProductName);
-                title.Append(" Ver.").Append(Application.ProductVersion);
+                title.Append(System.Windows.Forms.Application.ProductName);
+                title.Append(" Ver.").Append(System.Windows.Forms.Application.ProductVersion);
                 this.Text = title.ToString();
             }
             Properties.Settings.Default.PropertyChanged
@@ -102,122 +106,8 @@ namespace CameraTestTool
         /// <param name="evt">イベントオブジェクト</param>
         private void OnFormLoad(object sender, EventArgs evt)
         {
-
-#if false
-
-            try {
-                ApplySettingsToUI();
-
-                int captureDeviceCount = 0;
-                await Task.Run(() => {
-                    for (int i = 0; i < 20; i++) {
-                        VideoCapture vc = new VideoCapture();
-                        vc.Open(i);
-                        if (vc.IsOpened()) {
-                            vc.Dispose();
-                            captureDeviceCount++;
-                        } else {
-                            break;
-                        }
-                    }
-                });
-                Properties.ApplicationData.Default.CaptureDeviceCount = captureDeviceCount;
-
-                if (captureDeviceCount == 0) {
-                    throw new Exception("キャプチャデバイスがありません。");
-                }
-                if (Properties.Settings.Default.CaptureDeviceNo >= captureDeviceCount) {
-                    Properties.Settings.Default.CaptureDeviceNo = captureDeviceCount - 1;
-                }
-
-                UpdateCaptureDeviceCount();
-            }
-            catch (AggregateException e) {
-                StringBuilder msg = new StringBuilder();
-                foreach (Exception ie in e.InnerExceptions) {
-                    msg.Append(ie.Message).Append('\n');
-                }
-                MessageBox.Show(this, msg.ToString());
-            }
-            catch (Exception e) {
-                MessageBox.Show(this, e.Message);
-            }
-#endif
         }
 
-        private void ApplySettingsToUI()
-        {
-#if false
-            // 収縮強度設定値をUIに反映させる。
-            int contractionStrength = Properties.Settings.Default.ContractionStrength;
-            labelContractionStrength.Text = contractionStrength.ToString();
-            if (contractionStrength > trackBarContractionStrength.Maximum) {
-                contractionStrength = trackBarContractionStrength.Maximum;
-                Properties.Settings.Default.ContractionStrength = contractionStrength;
-            } else if (contractionStrength < trackBarContractionStrength.Minimum) {
-                contractionStrength = trackBarContractionStrength.Minimum;
-                Properties.Settings.Default.ContractionStrength = contractionStrength;
-            }
-            trackBarContractionStrength.Value = contractionStrength;
-
-            // 表示画像設定をUIに反映させる。
-            int displayImageType = Properties.Settings.Default.DisplayImageType;
-            if (displayImageType < 0) {
-                displayImageType = 0;
-                Properties.Settings.Default.DisplayImageType = displayImageType;
-            } else if (displayImageType >= comboBoxDisplayImageType.Items.Count) {
-                displayImageType = comboBoxDisplayImageType.Items.Count - 1;
-                Properties.Settings.Default.DisplayImageType = displayImageType;
-            }
-            comboBoxDisplayImageType.SelectedIndex = displayImageType;
-
-            // 2値化閾値設定をUIに反映させる。
-            int binaryThresholdBlockSize = Properties.Settings.Default.BinaryThresholdBlockSize;
-            if ((binaryThresholdBlockSize & 0x1) == 0) {
-                binaryThresholdBlockSize--;
-            }
-            if (binaryThresholdBlockSize < 1) {
-                binaryThresholdBlockSize = 1;
-            } else if (binaryThresholdBlockSize > 99) {
-                binaryThresholdBlockSize = 99;
-            }
-            trackBarBinaryThresholdBlockSize.Value = (binaryThresholdBlockSize - 1) / 2;
-            labelBinaryThresholdBlockSize.Text = binaryThresholdBlockSize.ToString();
-
-            // 対象エリア
-            int targetArea = Properties.Settings.Default.TargetArea;
-            if (targetArea < trackBarTargetArea.Minimum) {
-                targetArea = trackBarTargetArea.Minimum;
-            } else if (targetArea > trackBarTargetArea.Maximum) {
-                targetArea = trackBarTargetArea.Maximum;
-            }
-            trackBarTargetArea.Value = targetArea;
-            label1TargetArea.Text = targetArea.ToString();
-#endif
-        }
-
-        /// <summary>
-        /// キャプチャデバイス数の表示更新を行う。
-        /// </summary>
-        private void UpdateCaptureDeviceCount()
-        {
-#if false
-            int captureDeviceCount = Properties.ApplicationData.Default.CaptureDeviceCount;
-            int captureDeviceNo = Properties.Settings.Default.CaptureDeviceNo;
-            labelCaptureDevice.Text = captureDeviceNo.ToString();
-            if (captureDeviceNo >= 0) {
-                // キャプチャデバイスあり
-                menuItemMeasureStart.Enabled = true;
-                menuItemMeasureStop.Enabled = true;
-                buttonStartStop.Enabled = true;
-            } else {
-                // キャプチャデバイスなし。
-                menuItemMeasureStart.Enabled = false;
-                menuItemMeasureStop.Enabled = false;
-                buttonStartStop.Enabled = false;
-            }
-#endif
-        }
 
         /// <summary>
         /// フォームを閉じる操作を受けたときに処理を行う。
@@ -226,10 +116,6 @@ namespace CameraTestTool
         /// <param name="evt">イベントオブジェクト</param>
         private void OnFormClosing(object sender, FormClosingEventArgs evt)
         {
-#if false
-            StopCapture();
-#endif
-
             try
             {
                 Properties.Settings.Default.Save();
@@ -263,8 +149,14 @@ namespace CameraTestTool
         /// </summary>
         private void StoreCaptureSettings()
         {
-            CaptureDevice.AutoExposure = (double)(numericUpDownAutoExposure.Value);
-            CaptureDevice.Exposure = (double)(numericUpDownExposure.Value);
+            if (checkBoxExposureControl.Checked)
+            {
+                CaptureDevice.AutoExposure = (double)(numericUpDownAutoExposure.Value);
+            }
+            if (checkBoxExposure.Checked)
+            {
+                CaptureDevice.Exposure = (double)(numericUpDownExposure.Value);
+            }
         }
 
         /// <summary>
@@ -319,22 +211,41 @@ namespace CameraTestTool
         {
             try
             {
-                using (var form = new FormSelectCamera())
-                {
-                    if (form.ShowDialog(this) != DialogResult.OK)
-                    {
-                        Close();
-                    }
-                    else
-                    {
-                        CaptureDevice = new VideoCapture(form.SelectedIndex);
-                        Text = form.SelectedName;
-                    }
-                }
+                ChangeDeviceProc();
             }
             catch (Exception ex)
             {
                 System.Diagnostics.Debug.WriteLine(ex);
+                Close();
+            }
+        }
+
+        /// <summary>
+        /// デバイス変更処理
+        /// </summary>
+        private void ChangeDeviceProc()
+        {
+            using (var form = new FormSelectCamera())
+            {
+                if (form.ShowDialog(this) != DialogResult.OK)
+                {
+                    Close();
+                }
+                else
+                {
+                    try
+                    {
+                        CaptureDevice?.Release();
+                        CaptureDevice?.Dispose();
+                        CaptureDevice = null;
+                    }
+                    catch (Exception ex)
+                    {
+                        System.Diagnostics.Debug.WriteLine(ex);
+                    }
+                    CaptureDevice = new VideoCapture(form.SelectedIndex);
+                    Text = form.SelectedName;
+                }
             }
         }
 
@@ -359,17 +270,71 @@ namespace CameraTestTool
                 System.Diagnostics.Debug.WriteLine(ex);
                 MessageBox.Show(this, ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
+            buttonSave.Enabled = (pictureBox.Image != null);
             buttonCapture.Enabled = true;
         }
 
         /// <summary>
-        /// カメラ設定がクリックされた
+        /// カメラ設定がクリックされた時に通知を受け取る。
         /// </summary>
         /// <param name="sender">送信元オブジェクト</param>
         /// <param name="e">イベントオブジェクト</param>
         private void OnButtonCameraSettingClick(object sender, EventArgs e)
         {
             CaptureDevice.Settings = 1; // 設定画面表示
+        }
+
+        /// <summary>
+        /// 保存ボタンがクリックされた時に通知を受け取る。
+        /// </summary>
+        /// <param name="sender">送信元オブジェクト</param>
+        /// <param name="e">イベントオブジェクト</param>
+        private void OnButtonSaveClick(object sender, EventArgs e)
+        {
+            if (saveFileDialog.ShowDialog(this) != DialogResult.OK)
+            {
+                return;
+            }
+
+            try
+            {
+                var image = pictureBox.Image;
+                System.Drawing.Imaging.ImageFormat imageFormat;
+                switch (saveFileDialog.FilterIndex)
+                {
+                    case 0: // ファイルフィルター0 はJPEG
+                        imageFormat = System.Drawing.Imaging.ImageFormat.Jpeg;
+                        break;
+                    case 1: // ファイルフィルター1 はPNG
+                        imageFormat = System.Drawing.Imaging.ImageFormat.Png;
+                        break;
+                    default:
+                        imageFormat = System.Drawing.Imaging.ImageFormat.Jpeg;
+                        break;
+                }
+                image.Save(saveFileDialog.FileName, imageFormat);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        /// <summary>
+        /// カメラ変更ボタンがクリックされた
+        /// </summary>
+        /// <param name="sender">送信元オブジェクト</param>
+        /// <param name="e">イベントオブジェクト</param>
+        private void OnButtonChangeDevieClick(object sender, EventArgs e)
+        {
+            try
+            {
+                ChangeDeviceProc();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
         }
     }
 }
